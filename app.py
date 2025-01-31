8aa02dfbd21e36f87d4acd12401e5577fb66fdd3
Osamu Noguchi <noguchi@headwaters.co.jp>
2025-02-01 04:34:55 +0900

    [add] init

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-import timeit
+import timeit, math
 from multiprocessing import Process
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+
+WORKER_INDEXES = [0,1,2,3]
+PROC_LIST = list(range(10000000))
 
 def sample_fnc(process_index):
     print(f"process index: {process_index} started.")
     num = 0
 
-    for i in range(100000000):
+    for i in PROC_LIST:
+        num += 1
+    print(f"process index: {process_index} ended. {num}")
+
+def task(process_index):
+    print(f"process index: {process_index} started.")
+    num = 0
+
+    chunk_size = math.floor(len(PROC_LIST) / len(WORKER_INDEXES))
+    if process_index == 0:
+        chunk = PROC_LIST[0:chunk_size]
+    else:
+        chunk = PROC_LIST[chunk_size * process_index - 1: chunk_size * (process_index + 1)]
+    for i in chunk:
         num += 1
     print(f"process index: {process_index} ended. {num}")
 
@@ -26,12 +42,12 @@ def multiprocess():
         process.join()
 
 def processpool():
-    with ProcessPoolExecutor(max_workers=4) as e:
-        e.submit(sample_fnc, 1)
+    with ProcessPoolExecutor(max_workers=len(WORKER_INDEXES)) as e:
+        e.map(task, WORKER_INDEXES)
 
 def threadpool():
-    with ThreadPoolExecutor(max_workers=4) as e:
-        e.submit(sample_fnc, 1)
+    with ThreadPoolExecutor(max_workers=len(WORKER_INDEXES)) as e:
+        e.map(task, WORKER_INDEXES)
 
 if __name__ == '__main__':
     loop = 1
@@ -39,11 +55,11 @@ if __name__ == '__main__':
     result = timeit.timeit(lambda: singleprocess(), number=loop)
     print(f"single {result / loop}")
 
-    result = timeit.timeit(lambda: multiprocess(), number=loop)
-    print(f"multiprocess {result / loop}")
+    # result = timeit.timeit(lambda: multiprocess(), number=loop)
+    # print(f"multiprocess {result / loop}")
 
     result = timeit.timeit(lambda: processpool(), number=loop)
     print(f"processpool {result / loop}")
 
-    result = timeit.timeit(lambda: threadpool(), number=loop)
-    print(f"threadpool {result / loop}")
+    # result = timeit.timeit(lambda: threadpool(), number=loop)
+    # print(f"threadpool {result / loop}")
